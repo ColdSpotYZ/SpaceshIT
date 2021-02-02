28f0ec5e4176124d7d969211a8760bd73071d69d
Kai Xian Lim <s10195144@connect.np.edu.sg>
2021-02-03 00:39:02 +0800

    Added AVL Tree and BinaryNode for Maps ADT

--- SpaceshIT!/SpaceshIT!/Map.h
+++ SpaceshIT!/SpaceshIT!/Map.h
@@ -2,26 +2,124 @@
 
 #include<string>
 #include<iostream>
+using namespace std;
 
-typedef std::string ItemType;
-const int MAX_SIZE = 10;
+#include "BinaryNode.h"
+#include "AVL_Tree.h"	
 
+template<class T, class V>
 class Map
 {
 	private:
-		struct Node
-		{
-			int key;
-			ItemType value;
-			Node* next;
-			Node* prev;
-		};
-
-		Node* TopNode;
+		BinaryNode* root;
 
 	public:
-		Map();
+		Map<T, V>();
 
 		~Map();
+		
+		// 
+		bool insert(pair<T, V> pairarray[]);
+
+		bool erase(int interatorpos);
+
+		bool erase(V key);
+
+		T search(V key);
+
+		typename T::iterator begin();
+
+		typename T::iterator end();
+
+		bool isEmpty();
+
+		int size();
+
+		int getHeight();
+
+		int isBalanced();
+
+
 };
 
+///////////////////////////////
+///     IMPLEMENTATIONS     ///
+//////////////////////////////
+
+template<class T, class V>
+Map<T, V>::Map<T, V>()
+{
+	root = NULL;
+}
+
+template<class T, class V>
+Map<T, V>::~Map()
+{
+
+
+}
+
+template<class T, class V>
+bool Map<T, V>::insert(pair<T, V> pairarray[])
+{
+	if (!(V is int))
+	{
+
+	}
+}
+
+template<class T, class V>
+bool Map<T, V>::erase(int interatorpos)
+{
+
+}
+
+template<class T, class V>
+bool Map<T, V>::erase(V key)
+{
+
+}
+
+template<class T, class V>
+T Map<T, V>::search(V key)
+{
+
+}
+
+template<class T, class V>
+typename T::iterator Map<T, V>::begin()
+{
+	return this->root;
+}
+
+template<class T, class V>
+typename T::iterator Map<T, V>::end()
+{
+	return this->root + this->size;
+}
+
+
+
+template<class T, class V>
+bool Map<T, V>::isEmpty()
+{
+
+}
+
+template<class T, class V>
+int Map<T, V>::size()
+{
+	return size;
+}
+
+template<class T, class V>
+int Map<T, V>::getHeight()
+{
+
+}
+
+template<class T, class V>
+int Map<T, V>::isBalanced()
+{
+
+}
