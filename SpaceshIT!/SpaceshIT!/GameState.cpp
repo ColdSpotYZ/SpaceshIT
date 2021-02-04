474b36f45a4c21b68c87e12bbbaee7ef1d98ae00
ColdSpotYZ <jasonchua0808@gmail.com>
2021-02-04 19:05:04 +0800

    Quick fix for player collision

--- SpaceshIT!/SpaceshIT!/GameState.cpp
+++ SpaceshIT!/SpaceshIT!/GameState.cpp
@@ -114,19 +114,19 @@ void GameState::updateCollision()
 	{
 		if (this->playerVec->at(i)->getBounds().left < 0.f)
 		{
-			this->playerVec->at(i)->setPos(0.f, this->playerVec->at(i)->getBounds().top);
+			this->playerVec->at(i)->setPos(0.f + this->playerVec->at(i)->getBounds().width / 2.f, this->playerVec->at(i)->getBounds().top + this->playerVec->at(i)->getBounds().height / 2.f);
 		}
 		else if (this->playerVec->at(i)->getBounds().left + this->playerVec->at(i)->getBounds().width > this->window->getSize().x)
 		{
-			this->playerVec->at(i)->setPos(this->window->getSize().x - this->playerVec->at(i)->getBounds().width, this->playerVec->at(i)->getBounds().top);
+			this->playerVec->at(i)->setPos(this->window->getSize().x - this->playerVec->at(i)->getBounds().width / 2.f, this->playerVec->at(i)->getBounds().top + this->playerVec->at(i)->getBounds().height / 2.f);
 		}
 		if (this->playerVec->at(i)->getBounds().top < 0.f)
 		{
-			this->playerVec->at(i)->setPos(this->playerVec->at(i)->getBounds().left, 0.f);
+			this->playerVec->at(i)->setPos(this->playerVec->at(i)->getBounds().left + this->playerVec->at(i)->getBounds().width / 2.f, 0.f + this->playerVec->at(i)->getBounds().height / 2.f);
 		}
 		else if (this->playerVec->at(i)->getBounds().top + this->playerVec->at(i)->getBounds().height >= this->window->getSize().y)
 		{
-			this->playerVec->at(i)->setPos(this->playerVec->at(i)->getBounds().left, this->window->getSize().y - this->playerVec->at(i)->getBounds().height);
+			this->playerVec->at(i)->setPos(this->playerVec->at(i)->getBounds().left + this->playerVec->at(i)->getBounds().width / 2.f, this->window->getSize().y - this->playerVec->at(i)->getBounds().height / 2.f);
 		}
 	}
 }
